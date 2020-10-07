d8b1f4dadf7128ba4f16c6c789af5ff274fad8b9
Alisson-developer <alissonlua7@gmail.com>
2020-10-07 02:09:15 -0300

    Transpilação efetuada

---
+++ dist/knexfile.js
@@ -0,0 +1,53 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Update with your config settings.
+var path_1 = __importDefault(require("path"));
+var env = {
+    development: {
+        client: "sqlite3",
+        connection: {
+            filename: path_1.default.resolve(__dirname, 'src', 'database', 'database.sqlite')
+        },
+        migrations: {
+            directory: path_1.default.resolve(__dirname, 'src', 'database', 'migrations')
+        },
+        seeds: {
+            directory: path_1.default.resolve(__dirname, 'src', 'database', 'seeds')
+        },
+        useNullAsDefault: true
+    },
+    staging: {
+        client: "postgresql",
+        connection: {
+            database: "my_db",
+            user: "username",
+            password: "password"
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: "knex_migrations"
+        }
+    },
+    production: {
+        client: "postgresql",
+        connection: {
+            database: "my_db",
+            user: "username",
+            password: "password"
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: "knex_migrations"
+        }
+    }
+};
+exports.default = env;
